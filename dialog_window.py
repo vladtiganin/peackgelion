5e16c86cba5e63b3884e154a958ead86cb2a7b3e
Vlad <vladtiganin27@gmail.com>
2025-07-08 02:07:53 +0300

    goot night

--- dialog_window.py
+++ dialog_window.py
@@ -10,69 +10,61 @@ class Dialog:
         self.window = window
         self.active = True
 
-        self.surface = pygame.Surface((self.width, self.height), pygame.SRCALPHA)
+        # Создаем поверхность с прозрачностью для диалога
+        self.dialog_surface = pygame.Surface((self.width, self.height), pygame.SRCALPHA)
+        
 
-        self.background_color = pygame.Color(255, 195, 27)
-        self.border_color = pygame.Color(230, 169, 0)
-        self.text_color = pygame.Color('black')
-        self.text_font = pygame.font.SysFont('static/fonts/main_font.otf', 37)
+        self.background_color = (255, 195, 27, 200)  
+        self.border_color = (224, 120, 0, 255)       
+        self.text_color = (0, 0, 0, 255)             
 
+        self.text_font = pygame.font.Font('static/fonts/main_font.otf', 37)
         self.menu_font = pygame.font.Font('static/fonts/main_font.otf', 20)
         self.menu_text = self.menu_font.render('Menu', True, 'black')
-        self.menu_rect =  self.menu_text.get_rect(bottomright = (120, self.window.get_height() - 30))
+        self.menu_rect = self.menu_text.get_rect(bottomright=(120, self.window.get_height() - 30))
+
 
         self.imgBG = pygame.image.load('static/photo/bg_dialog_1.jpg')
         self.imgBG = pygame.transform.scale(self.imgBG, (self.window.get_width(), self.window.get_height()))
-
-
-
-    # def activate(self): 
-    #     self.activate = True
-    #     return True
-    
-
-    # def diactivate(self): 
-    #     self.activate = False
-    #     return False
-
+        self.imgMisato = pygame.image.load('static/photo/misato_ready.png')
 
     def draw(self):
-        if not self.active: return
+        if not self.active: 
+            return False
 
-        self.window.blit(self.imgBG, (0, 0)) 
 
-        self.imgMisato = pygame.image.load('static/photo/misato_ready.png')
-        rect = self.imgMisato.get_rect(center = (self.window.get_width() // 8 * 6, self.window.get_height() // 5 * 3)) 
-        self.window.blit(self.imgMisato, rect)
+        self.window.blit(self.imgBG, (0, 0))
+        misato_rect = self.imgMisato.get_rect(center=(self.window.get_width() // 8 * 6, self.window.get_height() // 5 * 3))
+        self.window.blit(self.imgMisato, misato_rect)
 
-        pygame.draw.rect(self.window, self.background_color, (self.x, self.y, self.width, self.height))
-        pygame.draw.rect(self.window, self.border_color, (self.x - 8, self.y - 8, self.width + 16, self.height + 16), 4)
-        pygame.draw.rect(self.window, 'black', (self.x - 4, self.y - 4, self.width + 8, self.height + 8), 4)
 
+        #self.dialog_surface.fill((0, 0, 0, 0))
+        
+        pygame.draw.rect(self.dialog_surface, self.background_color, (4, 4, self.width - 8, self.height - 8))
+        pygame.draw.rect(self.dialog_surface, self.border_color, (0, 0, self.width, self.height), 4)
+        
+
+        self.window.blit(self.dialog_surface, (self.x, self.y))
 
         text_x = self.x + 30
         text_y = self.y + 15
-        curentXpos = text_x
-        currentYpos = text_y
-        line_spacing = 25
+        current_x = text_x
+        current_y = text_y
 
         words = self.text.split(' ')
         for word in words:
-            word = self.text_font.render(word + ' ', 1, self.text_color)
-            if word.get_width() > self.width - curentXpos + 50:
-                currentYpos += word.get_height() + 5
-                curentXpos = text_x
+            word_surface = self.text_font.render(word + ' ', True, self.text_color)
+            if current_x + word_surface.get_width() > self.x + self.width - 30:
+                current_y += word_surface.get_height() + 5
+                current_x = text_x
 
-            self.window.blit(word , (curentXpos, currentYpos))
-            curentXpos += word.get_width()
+            self.window.blit(word_surface, (current_x, current_y))
+            current_x += word_surface.get_width()
 
+        self.window.blit(self.menu_text, self.menu_rect)
         
-
-        self.window.blit(self.menu_text, self.menu_rect)  
-
-        mouseX, mouseY = pygame.mouse.get_pos()
-        if self.menu_rect.collidepoint((mouseX, mouseY)):
-            border_rect = self.menu_rect.inflate(10, 10)
-            pygame.draw.rect(self.window, pygame.Color('black'), border_rect, 3)
+        mouse_pos = pygame.mouse.get_pos()
+        if self.menu_rect.collidepoint(mouse_pos):
+            pygame.draw.rect(self.window, pygame.Color('black'), self.menu_rect.inflate(10, 10), 3)
             return True
         return False
